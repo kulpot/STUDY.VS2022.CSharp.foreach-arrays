1331d1c2acd7a8b008b6da1b5d51342e2156b805
Kulpot <kulpotwahu@gmail.com>
2023-07-28 21:11:13 +0800

    Add project files.

---
+++ foreach arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+//ref link:https://www.youtube.com/watch?v=D8eR5kA-vUc&list=PLRwVmtr-pp07QlmssL4igw1rnrttJXerL&index=3
+// ctrl+k+c -- comment line
+
+class MainClass
+{
+    static void Main()
+    {
+        int[] ages = new[] { 25, 27, 23, 34, 36 };    
+        int totalAge = 0;
+        //for (int i = 0; i < ages.Length; i++)
+        foreach (int element in ages)
+        {
+            //Console.WriteLine(ages[i]);
+            //totalAge += ages[i];
+            Console.WriteLine(element);
+            totalAge += element;
+        }
+        Console.WriteLine(totalAge);
+        //ages[5] = 29; // out of range exception
+    }
+}
